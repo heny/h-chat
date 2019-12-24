fab7930e7e2e0192f31f0a2469b3aad4845e2938
何能勇 <nengyong.he-ext@ikang.com>
2019-12-24 18:09:28 +0800

    添加环境判断

--- my-chart/src/config.js
+++ my-chart/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mode: 'local',
+  mode: process.env.NODE_ENV === 'production' ? 'origin' : 'local',
   server: {
     local: '10.105.19.124:8888',
     origin: '39.107.82.176:8888'
