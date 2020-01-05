6e116cafeb8ccf1dbd15eab6866b5bc403ab9a63
heny <nengyong.he@ikang.com>
2020-01-05 14:56:46 +0800

    解决跨域问题

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ const io = require('socket.io')(server)
 
 const debug = require('debug')('my-application')
 
+
+app.use('/', (req,res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,OPTIONS');
+  next()
+})
+
 // 注意顺序  必须在msgRoutes之前挂载中间件
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -24,14 +32,6 @@ server.listen(port, _ => {
   debug('8888端口监听成功')
 })
 
-
-// 接收body参数
-app.use('/', (req,res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,OPTIONS');
-  next()
-})
 app.get('/',(req,res) => {
 	res.send('服务启动成功')
 })
